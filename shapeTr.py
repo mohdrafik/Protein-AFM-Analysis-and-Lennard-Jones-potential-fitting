3bd8e0248fcfa646e99710d4cddd71e7ec70eabe
mohdrafik <rafikiitbhu@gmail.com>
2024-09-08 18:24:23 +0200

    create the shape..

--- shapeTr.py
+++ shapeTr.py
@@ -9,3 +9,10 @@ def tree(height):
 if __name__ =="__main__":
     height = int(input("enter height of the triangle >= 2:"))
     tree(height)
+
+# import os
+# data_path = "E:\\Protein-AFM-Analysis-and-Lennard-Jones-potential-fitting\\data\\"
+# # datapath = "E:\\Protein-AFM-Analysis-and-Lennard-Jones-potential-fitting\\data\\processdata\\"
+# datapath1 = os.path.join(data_path,'processdata\\')
+# print("new------>",datapath1)
+# print(os.listdir(datapath1))
