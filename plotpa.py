a8edc9aafaf965b8034d542e264f1a2f3c930239
mohdrafik <rafikiitbhu@gmail.com>
2024-09-11 15:43:01 +0200

    no change

--- plotpa.py
+++ plotpa.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import os 
 
 # path = "E:\\python_programs\\xlsfileprocess\\dataproblem\\new_data7sep24\\processdata\\"
-path = "E:\\Protein-AFM-Analysis-and-Lennard-Jones-potential-fitting\\new_data7sep24\\processdata\\"
+# path = "E:\\Protein-AFM-Analysis-and-Lennard-Jones-potential-fitting\\data\\processdata\\"
+path = r"E:\Protein-AFM-Analysis-and-Lennard-Jones-potential-fitting\data\processdata"
+
 listfiles = os.listdir(path) 
 print(listfiles)
 
@@ -13,7 +15,9 @@ for file in listfiles:
     if file.endswith('.dat'):
         print(file)
         count = count +1
-        df = pd.read_csv(path+file,delimiter=' ')
+        filename = os.path.join(path,file)
+        # df = pd.read_csv(path+file,delimiter=' ')
+        df = pd.read_csv(filename,delimiter=' ')
         print(df.head())
         # plt.figure()
         plt.plot(df['piezo'],df['amplitude'])
@@ -21,8 +25,6 @@ plt.show()
         # if count ==1:
         #     break
         # plt.legend()
-        
-
 
 
 
